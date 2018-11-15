a525e0bbce7597d3d35fef9a1bb2867020ce982b
Jose Luis Martinez <jlmartinez@capside.com>
2018-11-15 23:10:44 +0100

    Commit all the images for the talk

---
+++ img_6/app.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+use Mojolicious::Lite;
+use HTTP::Tiny;
+
+my $ua = HTTP::Tiny->new;
+my $content = $ua->get('https://metacpan.org/recent')->{ content };
+
+my ($results) = ($content =~ m/(\d+,\d+)/);
+
+get '/' => {text => "There are $results"};
+
+app->start;
